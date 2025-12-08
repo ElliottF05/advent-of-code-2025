88ee1f1eea5dc5f1ec40b20eb6f7007f6f696db6
ElliottF05 <elliotthfaa@gmail.com>
2025-12-08 11:32:59 -0500

    add execution time to main

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,5 @@
 let () = 
-    print_endline (Day8.Part2.main ())
+    let start_time = Sys.time() in
+    Printf.printf "Answer: %s\n" (Day8.Part2.main ());
+    let end_time = Sys.time() in
+    Printf.printf "Execution time: %f seconds\n" (end_time -. start_time);
