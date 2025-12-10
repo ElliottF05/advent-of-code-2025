143d47c0028ea1bc58f4a815c86b41b2cc70ca5e
ElliottF05 <elliotthfaa@gmail.com>
2025-12-09 22:42:39 -0500

    day9, rewrite part2 for 40x speedup, down to 23ms

--- bin/day9/part2.ml
+++ bin/day9/part2.ml
@@ -7,165 +7,140 @@ let line_to_pos line =
   | [x ; y] -> (Int.of_string x, Int.of_string y)
   | _ -> failwith "invalid line"
 
-let get_rect_size (pos1, pos2) = 
-  let x1, y1 = pos1 in
-  let x2, y2 = pos2 in
-  let dx = Int.abs(x2 - x1) + 1 in
-  let dy = Int.abs(y2 - y1) + 1 in
-  dx * dy
-
-let get_top_left_idx positions = 
-  let seed_index, _seed_x, _seed_y = positions
-  |> Array.foldi ~init:(-1, Int.max_value, Int.max_value) ~f:(fun i (j, px, py) (x, y) -> 
-    if y < py || (y = py && x < px) then
-      (i, x, y)
-    else
-      (j, px, py)
-    )
-  in
-  seed_index
-
-let get_out_dir positions i =
-  let n = Array.length positions in
-  let j = Int.(%) (i+1) n in
-
-  let x,y = positions.(i) in
-  let nx,ny = positions.(j) in
-
-  let dx = nx - x in
-  let dy = ny - y in
-
-  if dx = 0 then
-    if dy > 0 then (0, 1) else (0, -1)
-  else
-    if dx > 0 then (1,0) else (-1, 0)
-
-let get_in_dir positions i = 
-  let n = Array.length positions in
-  let j = Int.(%) (i-1) n in
-  get_out_dir positions j
-
-let cross dir1 dir2 = 
-  let dx1, dy1 = dir1 in
-  let dx2, dy2 = dir2 in
-  dx1*dy2 - dy1*dx2
-
-
-let get_outer_turn_sign positions top_left_idx = 
-  let in_dir = get_in_dir positions top_left_idx in
-  let out_dir = get_out_dir positions top_left_idx in
-  let c = cross in_dir out_dir in
-  if c < 0 then -1 else 1
-
-let get_corners positions outer_turn_sign = 
-  let n = Array.length positions in
-  List.range 0 n
-  |> List.map ~f:(fun i ->
-      let dx_in, dy_in = get_in_dir positions i in
-      let dx_out, dy_out = get_out_dir positions i in
-      let x,y = positions.(i) in
-
-      let cross_val = cross (dx_in, dy_in) (dx_out, dy_out) in
-      let is_outer_turn = outer_turn_sign * cross_val > 0 in
-
-      if is_outer_turn then
-        (x + dx_in - dx_out, y + dy_in - dy_out)
-      else
-        (x - dx_in + dx_out, y - dy_in + dy_out)
-    )
+let get_idx_to_pos_maps positions = 
+  let x_positions = positions
+  |> Array.map ~f:(fun (x,_y) -> x)
+  |> Array.to_list
+  |> List.dedup_and_sort ~compare:Int.compare
   |> List.to_array
+  in
 
-let between a b x = 
-  let lo = Int.min a b in
-  let hi = Int.max a b in
-  lo <= x && x <= hi
+  let y_positions = positions
+  |> Array.map ~f:(fun (_x,y) -> y)
+  |> Array.to_list
+  |> List.dedup_and_sort ~compare:Int.compare
+  |> List.to_array
+  in
 
-let ranges_overlap a1 a2 b1 b2 = 
-  let lo1 = Int.min a1 a2 in
-  let hi1 = Int.max a1 a2 in
-  let lo2 = Int.min b1 b2 in
-  let hi2 = Int.max b1 b2 in
-  lo1 <= hi2 && lo2 <= hi1
+  x_positions, y_positions
 
-let is_horizontal (x1, y1) (x2, y2) =
-  y1 = y2 && x1 <> x2
+let get_pos_to_idx_map arr = 
+  let map = List.range 0 (Array.length arr)
+  |> Hashtbl.create_mapped (module Int) ~growth_allowed:false ~get_key:(fun i -> arr.(i)) ~get_data:(fun i -> i)
+  in
 
-let lines_intersect (ax1, ay1) (ax2, ay2) (bx1, by1) (bx2, by2) = 
-  let a_h = is_horizontal (ax1, ay1) (ax2, ay2) in
-  let b_h = is_horizontal (bx1, by1) (bx2, by2) in
-  match a_h, b_h with 
-  | true, true -> 
-    (* both horizontal *)
-    ay1 = by1 && (ranges_overlap ax1 ax2 bx1 bx2)
-  | false, false ->
-    (* both vertical *)
-    ax1 = bx1 && (ranges_overlap ay1 ay2 by1 by2)
-  | true, false ->
-    (* a is horizontal, b is vertical *)
-    (between ax1 ax2 bx1) && (between by1 by2 ay1)
-  | false, true ->
-    (* a is vertical, b is horizontal *)
-    (between ay1 ay2 by1) && (between bx1 bx2 ax1)
+  match map with
+  | `Ok x -> x
+  | _ -> failwith "duplicate keys"
 
-let rect_edges (x1, y1) (x2, y2) = 
-  [
-    ((x1, y1), (x1, y2)) ;
-    ((x1, y1), (x2, y1)) ;
-    ((x2, y2), (x1, y2)) ;
-    ((x2, y2), (x2, y1)) ;
-  ]
- 
-let rectangle_intersects_line (corner1, corner2) (line_start, line_end) = 
-  let edges = rect_edges corner1 corner2 in
-  edges
-  |> List.exists ~f:(fun (e1, e2) -> lines_intersect e1 e2 line_start line_end)
+let get_idx x_to_col y_to_row (x, y) = 
+  Hashtbl.find_exn x_to_col x, Hashtbl.find_exn y_to_row y
 
+let fill_edge grid (c1, r1) (c2, r2) = 
+  if c1 = c2 then
+    List.range ~stop:`inclusive (Int.min r1 r2) (Int.max r1 r2)
+    |> List.iter ~f:(fun r -> grid.(c1).(r) <- true)
+  else
+    List.range ~stop:`inclusive (Int.min c1 c2) (Int.max c1 c2)
+    |> List.iter ~f:(fun c -> grid.(c).(r1) <- true)
 
-let rectangle_is_valid corner_pairs rectangle =
-  not (Array.exists ~f:(rectangle_intersects_line rectangle) corner_pairs)
+let fill_grid_edges grid positions x_to_col y_to_row = 
+  List.range 0 (Array.length positions)
+  |> List.iter ~f:(fun i -> 
+    let j = Int.(%) (i+1) (Array.length positions) in
+    let idx1 = get_idx x_to_col y_to_row positions.(i) in
+    let idx2 = get_idx x_to_col y_to_row positions.(j) in
+    fill_edge grid idx1 idx2;
+    )
 
-let main () = 
-  let lines = In_channel.read_lines file in
+let find_seed_cell grid = 
+  let rec scan c r = 
+    if r >= (Array.length grid.(0)) then
+      scan (c+1) 0
+    else
+      if grid.(c).(r) then
+        c+1,r+1
+      else
+        scan c (r+1)
+  in
+  scan 0 0
 
-  let positions = lines
+let in_bounds grid col row = 
+  0 <= col && col < (Array.length grid) && 0 <= row && row < Array.length grid.(0)
+  
+let rec flood_fill grid col row = 
+  if (in_bounds grid col row) && (not grid.(col).(row)) then
+    begin
+      grid.(col).(row) <- true;
+      flood_fill grid (col+1) row;
+      flood_fill grid (col-1) row;
+      flood_fill grid col (row+1);
+      flood_fill grid col (row-1);
+    end
+
+let build_prefix prefix grid = 
+  List.range 0 (Array.length grid)
+  |> List.iter ~f:(fun c ->
+    List.range 0 (Array.length grid.(0))
+    |> List.iter ~f:(fun r ->
+        let curr_val = if grid.(c).(r) then 1 else 0 in
+        prefix.(c+1).(r+1) <- prefix.(c+1).(r) + prefix.(c).(r+1) - prefix.(c).(r) + curr_val;
+      )
+    )
+
+let normalize_coords (x1, y1) (x2, y2) = 
+  let left = Int.min x1 x2 in
+  let right = Int.max x1 x2 in
+  let top = Int.min y1 y2 in
+  let bottom = Int.max y1 y2 in
+  (left, top), (right, bottom)
+
+let get_rect_area (x1, y1) (x2, y2) = 
+  (x2 - x1 + 1) * (y2 - y1 + 1)
+
+let get_rect_sum prefix (c1, r1) (c2, r2) = 
+  prefix.(c2+1).(r2+1) - prefix.(c1).(r2+1) - prefix.(c2+1).(r1) + prefix.(c1).(r1)
+
+let main () : string = 
+  let positions = In_channel.read_lines file
   |> List.map ~f:line_to_pos
   |> List.to_array
   in
 
-  let top_left_idx = get_top_left_idx positions in
-  let outer_turn_sign = get_outer_turn_sign positions top_left_idx in
-  let corners = get_corners positions outer_turn_sign in
+  let col_to_x, row_to_y = get_idx_to_pos_maps positions in
+  let x_to_col = get_pos_to_idx_map col_to_x in
+  let y_to_row = get_pos_to_idx_map row_to_y in
 
-  let corner_pairs = List.range 0 (Array.length corners)
-  |> List.map ~f:(fun i ->
-    let j = Int.(%) (i+1) (Array.length corners) in
-    (corners.(i), corners.(j))
+  let rows = Array.length row_to_y in
+  let cols = Array.length col_to_x in
+
+  let grid = Array.make_matrix ~dimx:cols ~dimy:rows false in
+  fill_grid_edges grid positions x_to_col y_to_row;
+
+  let seed_c, seed_r = find_seed_cell grid in
+  flood_fill grid seed_c seed_r;
+
+  let prefix = Array.make_matrix ~dimx:(cols+1) ~dimy:(rows+1) 0 in
+  build_prefix prefix grid;
+
+  let result = ref 0 in
+  Array.cartesian_product positions positions 
+  |> Array.iter ~f:(fun (unnormalized_pos1, unnormalized_pos2) ->
+    let pos1, pos2 = normalize_coords unnormalized_pos1 unnormalized_pos2 in
+    let full_area = get_rect_area pos1 pos2 in
+    if full_area > !result then
+      begin
+        let idx1 = get_idx x_to_col y_to_row pos1 in
+        let idx2 = get_idx x_to_col y_to_row pos2 in
+        let reduced_area = get_rect_area idx1 idx2 in
+        let covered_area = get_rect_sum prefix idx1 idx2 in
+        if covered_area = reduced_area then
+          result := full_area;
+      end
     )
-  |> List.to_array
-  in
+  ;
 
-  let range = List.range 0 (Array.length positions) in
-  let rectangles = List.cartesian_product range range
-  |> List.filter ~f:(fun (i,j) -> i < j)
-  |> List.map ~f:(fun (i,j) -> (positions.(i), positions.(j)))
-  |> List.to_array
-  in
-  
-  let valid_rectangles = rectangles
-  |> Array.filter ~f:(rectangle_is_valid corner_pairs)
-  in
-
-  let best_rect_opt = valid_rectangles
-  |> Array.max_elt ~compare:(fun r1 r2 -> Int.compare (get_rect_size r1) (get_rect_size r2))
-  in
-
-  let best_rect = match best_rect_opt with
-  | Some r -> r
-  | None -> failwith "no valid rectangles found"
-  in
-
-
-  Int.to_string (get_rect_size best_rect)
+  Int.to_string !result
 
 (* 4_629_483_216 is too high *)
 (* 1_525_991_432 is correct *)
